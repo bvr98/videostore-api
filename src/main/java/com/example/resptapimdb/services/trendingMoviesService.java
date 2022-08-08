04a76eff7671fa03deac9e73ca268d19f7d72b7a
Venkat Reddy <bathinivenkat212@gmail.com>
2022-08-08 11:33:25 -0400

    crossOrigin

--- src/main/java/com/example/resptapimdb/services/trendingMoviesService.java
+++ src/main/java/com/example/resptapimdb/services/trendingMoviesService.java
@@ -2,7 +2,6 @@ package com.example.resptapimdb.services;
 
 import com.example.resptapimdb.models.trendingMovies;
 import com.example.resptapimdb.models.trendingMoviesRepository;
-import com.example.resptapimdb.models.trendingMoviesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
